1efa04cd198b49490853d877c47d577379a74fc6
KAJAL <kajal@krishaweb.net>
2016-01-11 19:28:18 +0530

    changes

--- resources/views/layouts/login.blade.php
+++ resources/views/layouts/login.blade.php
@@ -36,11 +36,6 @@
 
   <script src="{{ elixir('js/vendor.js') }}"></script>
 	<script src="{{ elixir('js/app.js') }}"></script>
-	<script type="text/javascript">
-    $(function()
-    {
-      $('#form-login').validate()
-    })
-    </script>
+	
   </body>
 </html>
