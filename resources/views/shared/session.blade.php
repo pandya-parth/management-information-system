c84c1653b7e0a7e6017c3bd9516fb532385e7494
KAJAL <kajal@krishaweb.net>
2016-01-12 14:48:00 +0530

    changes

---
+++ resources/views/shared/session.blade.php
@@ -0,0 +1,37 @@
+<div class="container" id="messages">
+
+@if(Session::has('status'))
+<div class="alert alert-danger ">
+  <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+  @if (session('status'))
+      <div class="alert alert-danger">
+          {{ session('status') }}
+      </div>
+  @endif 
+</div>
+@endif
+
+
+@if(Session::has('success'))
+<div class="alert alert-success ">
+  <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+  @if(Session::get('success') != 1)
+    {{ Session::get('success') }}
+  @else
+    An e-mail with the password reset has been sent.
+  @endif    
+</div>
+@endif
+
+
+@if(Session::has('error'))
+<div class="alert alert-danger">
+  <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+  @if(Session::get('error') != 1)
+    {{ Session::get('error') }}
+  @else
+   {{ trans(Session::get('reason')) }}
+  @endif
+</div>
+@endif
+</div>
