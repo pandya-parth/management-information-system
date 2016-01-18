05185a21ff7cbbe049f9689a120315a4668cb362
Baldev <baldev@krishaweb.net>
2016-01-18 10:36:33 +0530

    Task Categories add

--- resources/views/task_categories/index.blade.php
+++ resources/views/task_categories/index.blade.php
@@ -1,7 +1,69 @@
 @extends('layouts.app')
 @section('title','Task Categories')
 @section('content')
+
+ <div class="modal fade slide-up disable-scroll" id="my-modal" tabindex="-1" role="dialog" aria-hidden="false" >
+      <div class="modal-dialog ">
+        <div class="modal-content-wrapper">
+          <div class="modal-content">
+            <div class="modal-header clearfix text-left">
+              <button type="button" class="close" data-dismiss="modal" aria-hidden="true"><i class="pg-close fs-14"></i>
+              </button>
+              <h5>Payment <span class="semi-bold">Information</span></h5>
+              <p class="p-b-10">We need payment information inorder to process your order</p>
+            </div>
+            <div class="modal-body">
+              <form role="form">
+                <div class="form-group-attached">
+                  <div class="row">
+                    <div class="col-sm-12">
+                      <div class="form-group form-group-default">
+                        <label>Company Name</label>
+                        <input type="email" class="form-control">
+                      </div>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-sm-8">
+                      <div class="form-group form-group-default">
+                        <label>Card Number</label>
+                        <input type="text" class="form-control">
+                      </div>
+                    </div>
+                    <div class="col-sm-4">
+                      <div class="form-group form-group-default">
+                        <label>Card Holder</label>
+                        <input type="text" class="form-control">
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </form>
+              <div class="row">
+                <div class="col-sm-8">
+                  <div class="p-t-20 clearfix p-l-10 p-r-10">
+                    <div class="pull-left">
+                      <p class="bold font-montserrat text-uppercase">TOTAL</p>
+                    </div>
+                    <div class="pull-right">
+                      <p class="bold font-montserrat text-uppercase">$20.00</p>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-sm-4 m-t-10 sm-m-t-10">
+                  <button type="button" class="btn btn-primary btn-block m-t-5">Pay Now</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- /.modal-content -->
+      </div>
+    </div>
+
+
 <div class= "content">
+
 <!-- START CONTAINER FLUID -->
           <div class="container-fluid container-fixed-lg">
             <!-- START PANEL -->
@@ -11,8 +73,9 @@
                 </div>
                 <div class="pull-right">
                   <div class="col-xs-12">
-                    {!! link_to("project/create","Add",array('class'=>'btn btn-primary btn-cons   ')) !!}
-<!-- <button id="show-modal" class="btn btn-primary btn-cons"><i class="fa fa-plus"></i> Add New</button> -->
+                    
+                    <a class="btn" data-controls-modal="my-modal" keyboard=true data-backdrop="static">Launch Modal</a>
+                   <!-- <button id="show-modal" class="btn btn-primary btn-cons"><i class="fa fa-plus"></i> Add New</button -->
                   </div>
                 </div>
                 <div class="clearfix"></div>
