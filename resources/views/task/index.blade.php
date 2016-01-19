1ebfd8814eb986b82961b44fa0b4437ac105b5ac
Baldev <baldev@krishaweb.net>
2016-01-19 18:09:37 +0530

    task add

--- resources/views/task/index.blade.php
+++ resources/views/task/index.blade.php
@@ -281,7 +281,7 @@
           </div>
               <div class="modal-footer">
                 <button id="add-app" type="button" class="btn btn-primary  btn-cons">Add</button>
-                <button type="button" class="btn btn-cons" id="close_btn" data-dismiss="modal" aria-hidden="true">Close</button>
+                <button type="button" class="btn btn-cons" id="close" data-dismiss="modal">Close</button>
               </div>
             </div>
           </div>
