5bc14932de250173e100be2fe60fb4a4082d43ea
KAJAL <kajal@krishaweb.net>
2016-01-22 10:56:24 +0530

    sluggable

--- app/ProjectCategory.php
+++ app/ProjectCategory.php
@@ -3,9 +3,17 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\SluggableInterface;
+use Cviebrock\EloquentSluggable\SluggableTrait;
 
-class ProjectCategory extends Model
+class ProjectCategory extends Model implements SluggableInterface
 {
  	protected $table='project_categories';
  	protected $fillable=['name'];
+ 	use SluggableTrait;
+
+    protected $sluggable = [
+        'build_from' => 'name',
+        'save_to'    => 'slug',
+    ];
 }
