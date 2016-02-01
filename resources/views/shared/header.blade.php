8c8c581fcda61a321aa87134b513844a0d06d6e1
KAJAL <kajal@krishaweb.net>
2016-02-01 14:20:09 +0530

    chnages in header

--- resources/views/shared/header.blade.php
+++ resources/views/shared/header.blade.php
@@ -29,6 +29,11 @@
             </div>
         </div>
         <div class=" pull-right">
+          <div class="header-inner">
+            <a href="#" class="btn-link icon-set menu-hambuger-plus m-l-20 sm-no-margin hidden-sm hidden-xs" data-toggle="quickview" data-toggle-element="#quickview"></a>
+          </div>
+        </div>
+        <div class=" pull-right">
           <!-- START User Info-->
           <div class="visible-lg visible-md m-t-10">
             <div class="pull-left p-r-10 p-t-10 fs-16 font-heading">
@@ -60,4 +65,5 @@
           </div>
           <!-- END User Info-->
         </div>
+
       </div>
