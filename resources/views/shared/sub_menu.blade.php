df53097df23a0e9053ef894fbb84f21f55149a3b
Baldev <baldev@krishaweb.net>
2016-02-05 17:41:31 +0530

    route issue

--- resources/views/shared/sub_menu.blade.php
+++ resources/views/shared/sub_menu.blade.php
@@ -1,12 +1,12 @@
 
 	<ul class="nav navbar-nav">
-        <li class="active"><a href="{!! url('companies') !!}">Company <span class="sr-only">(current)</span></a></li>
-        <li><a href="{!! url('people') !!}">People</a></li>
+        <li class="active"><a href="{!! url('/companies') !!}">Company <span class="sr-only">(current)</span></a></li>
+        <li><a href="{!! url('/people') !!}">People</a></li>
         <li class="dropdown">
         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"> Project <span class="caret"></span></a>
           <ul class="dropdown-menu">
-            <li><a href="{!! url('project-categories') !!}">Project Category</a></li>
-            <li><a href="{!! url('projects') !!}">Project</a></li>
+            <li><a href="{!! url('/project-categories') !!}">Project Category</a></li>
+            <li><a href="{!! url('/projects') !!}">Project</a></li>
           </ul>
         </li>
       </ul>
