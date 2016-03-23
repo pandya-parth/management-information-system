0e6957824b6e4da86a4508f12dac90995e04beea
KAJAL <kajal@krishaweb.net>
2016-03-23 17:16:26 +0530

    changes in header

--- resources/views/tasks/everything.blade.php
+++ resources/views/tasks/everything.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.everything')
+@extends('layouts.app')
 @section('title','Task')
 @section('content')
 <div ng-controller="TasksCtrl">
@@ -25,77 +25,76 @@
           <p class="text-center" ng-show="loading"><img src="{!! asset('img/demo/progress.svg') !!}" /></p>
           <div ng-cloak class="panel-group"  role="tablist" aria-multiselectable="true" >
             
-            @foreach($task_categories as $category)
+            
               
             <div  class="accordion">
-              <div ng-cloak class="panel-heading everything-header" role="tab" id="headingOne" >
-                <h4 class="panel-title">
-                  <a>
-                    {!! $category->name !!}
-                  </a>
-                </h4>
-              </div>
-              <div id="tasklist{!! $category->id !!}" class="panel-collapse collapse in everything-column" role="tabpanel"  aria-labelledby="headingOne" aria-expanded="false">
+              <div class="panel-collapse collapse in everything-column" role="tabpanel"  aria-labelledby="headingOne" aria-expanded="false">
                 <div class="panel-body">
 
 <div ng-cloak class="grid_list_view everything-column-inner">
                             <div ng-cloak class="head list_view border_class">
                                 <div ng-cloak class="row">
                                     <div class="datas people_name">Project Name</div>
-                                    <div class="datas people_name">Task Name</div>
-                                    <div class="datas people_name">Notes</div>
+                                    <div class="datas people_name">User</div>
+                                    <div class="datas people_name">Description</div>
                                     <div class="datas people_name">Category Name</div>
-                                    <div class="datas people_name">Start Date</div>
-                                    <div class="datas people_name">Due Date</div>
+                                    <div class="datas people_name">Date</div>
+                                    <div class="datas people_name">Start</div>
+                                    <div class="datas people_name">End</div>
                                     <div class="datas people_name">Action</div>
                                 </div>
                             </div>
                           </div>
                           
-                          @foreach($tasks as $task)
+
+                          @foreach($logtimes as $logtime)
                           
-                    @if($category->id == $task->category_id)
+                    
                     <div  class="grid_list_view">
                             
                             <div class="data_area list_view ">
                                 <!-- row 1 -->
                                 <div  class="row border_class">
                                     <div  class="datas people_designation">
-                                        {!! $task->project->name !!}
-                                    </div>
-                                    <div  class="datas people_email">
-                                        {!! $task->name !!}
-                                    </div>
-                                    <div  class="datas people_phone">
-                                        {!! $task->notes !!}
+                                        {!! $logtime->task->project->name !!}
                                     </div>
                                     <div  class="datas people_designation">
-                                        {!! $task->category->name !!}
+                                        {!! $logtime->user->people->name !!}
                                     </div>
                                     <div  class="datas people_email">
-                                        {!! $task->start_date !!}
+                                        {!! $logtime->task->name !!}--{!! $logtime->description !!}
+                                    </div>
+                                    <div  class="datas people_designation">
+                                        {!! $logtime->task->category->name !!}
+                                    </div>
+                                    <div  class="datas people_email">
+                                        {!! $logtime->date !!}
+                                    </div>
+                                    <div  class="datas people_email">
+                                        {!! $logtime->start_time !!}
                                     </div>
                                     <div  class="datas people_phone">
-                                        {!! $task->due_date !!}
+                                        {!! $logtime->end_time !!}
                                     </div>
                                     <div class="task_detail">
-                                      <a href="{!!url('/projects'),'/',$task->project_id,'/tasks','/',$task->id !!}" class="timer timer_button"  id="view_button1">
+                                      <a href="{!!url('/projects'),'/',$logtime->task->project_id,'/tasks','/',$logtime->task->id !!}" class="timer timer_button"  id="view_button1">
                                         <i class="fa fa-eye"></i>
                                       </a>
                                     </div>
+                                </div>
                                 </div>
                                 <!-- row 1 complete -->
                             </div>
                         </div>
 
-                        @endif
+                        
                       
                     @endforeach     
                                  
                 </div>
               </div>
             </div>
-            @endforeach
+            
             
           </div>
         </div>
